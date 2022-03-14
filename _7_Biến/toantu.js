8004bd1288d5251868024e9c73296ccdf9c03c24
unknown <chuongtep113@gmail.com>
2022-03-14 22:20:42 +0700

    daylile

---
+++ _7_Biến/toantu.js
@@ -0,0 +1,8 @@
+let inputwidth;
+let inputheight;
+inputwidth=prompt("Enter the width");
+inputheight= prompt("Enter the height");
+wigth = +(inputwidth);
+height = +(inputheight);
+let area = wigth * height;
+document.write("The area is :" +area)
